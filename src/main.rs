6365031755522bbf8765adad140623323129847a
Ashton Snelgrove <ashton@snelgrove.science>
2019-12-04 12:26:12 -0700

    Fix basis presets

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ mod keycodes;
 mod points;
 
 use structopt::StructOpt;
+use structopt::clap::ArgGroup;
 use points::{BasisVector, Interval, point_to_interval};
 use keycodes::key_to_point;
 use glfw::{Action, Context, Key};
-use midir::{MidiOutput, MidiOutputPort};
-use std::io::stdin;
+use midir::MidiOutput;
 
 // something else interesting [1, 3, -90] haha reinvented b-system in a different orientation
 // Bosanquet diagram [3, 2, 0]
@@ -23,15 +23,15 @@ use std::io::stdin;
 // Park [5, 2, 0]
 // Wesley [7, -5, 0]
 // Fernandez [6, 7, -90]
-// Wicki-Haydn [5, 7, -90]
+// Wicki-Hayden [5, 7, -90]
 // B-system [-2, 1, -90]
 // C-system [-1, 2, -90]
 
 #[derive(StructOpt)]
 struct Basis {
-    #[structopt(short, long, default_value = "4")]
+    #[structopt(short = "a", long, default_value = "4")]
     upper: i8,
-    #[structopt(short, long, default_value = "-3")]
+    #[structopt(short = "b", long, default_value = "-3")]
     lower: i8,
     #[structopt(short, long, default_value = "-90")]
     rotation: i8
@@ -41,22 +41,35 @@ const JANKO: Basis            = Basis { upper: -1, lower: 1,  rotation: -90 };
 const INVERTED_GERHARD: Basis = Basis { upper: 3,  lower: 4,  rotation: -90 };
 const GERHARD: Basis          = Basis { upper: 4,  lower: 3,  rotation: 0 };
 const TONNETZ: Basis          = Basis { upper: 4,  lower: -3, rotation:  0 };
-const GUITAR_A: Basis         = Basis { upper: -1, lower: 1,  rotation: -90 };
-const GUITAR_B: Basis         = Basis { upper: -1, lower: 1,  rotation: -90 };
+const GUITAR_A: Basis         = Basis { upper: 4,  lower: 5,  rotation: -90 };
+const GUITAR_B: Basis         = Basis { upper: 5,  lower: 6,  rotation: -90 };
 const PARK: Basis             = Basis { upper: 5,  lower: 2,  rotation: -90 };
 const WESLEY: Basis           = Basis { upper: 7,  lower: -5, rotation: 0};
 const FERNANDEZ: Basis        = Basis { upper: 6,  lower: 7,  rotation: -90 };
-const WICKI_HAYDN: Basis      = Basis { upper: 5,  lower: 7,  rotation: -90 };
-const B_SYSTEM: Basis         = Basis { upper: -2, lower: 1,  rotation: -90 };
-const C_SYSTEM: Basis         = Basis { upper: -1, lower: 2,  rotation: -90 };
+const WICKI_HAYDEN: Basis     = Basis { upper: 5,  lower: 7,  rotation: -90 };
+const C_SYSTEM: Basis         = Basis { upper: -2, lower: 1,  rotation: -90 };
+const B_SYSTEM: Basis         = Basis { upper: -1, lower: 2,  rotation: -90 };
+const BOSANQUET: Basis        = Basis { upper: 3,  lower: 2, rotation: 0 };
 const NAMES: &'static [&'static str] = &["C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B"];
+const PRESETS: &'static [&'static str] = &["janko", "inverted_gerhard", "gerhard", "tonnetz", "guitar_a", "guitar_b", "park", "wesley", "fernandez", "wicki-hayden", "c_system", "b_system", "bosanquet"];
 
 #[derive(StructOpt)]
 struct BasisPreset {
-    #[structopt(short, long)]
+    #[structopt(short, long, possible_values = PRESETS)]
     preset: Option<String>,
     #[structopt(flatten)]
     specified: Basis,
+}
+
+#[derive(StructOpt)]
+#[structopt(group = ArgGroup::with_name("midi").required(true))]
+struct MidiConfig {
+    #[structopt(short = "v", long = "virtual", group = "midi")]
+    use_virtual: bool,
+    #[structopt(short = "l", long = "list", group = "midi")]
+    list_outputs: bool,
+    #[structopt(short, long, group = "midi")]
+    output: Option<String>
 }
 
 #[derive(StructOpt)]
@@ -64,35 +77,44 @@ struct BasisPreset {
 struct Opts {
     #[structopt(flatten)]
     basis: BasisPreset,
+    #[structopt(flatten)]
+    midi: MidiConfig,
     #[structopt(short, long, default_value = "0")]
     transpose: i8,
 }
 
 fn main() {
     let opts: Opts = Opts::from_args();
-
-    let midi_out = MidiOutput::new("My Test Output").unwrap();
-    let out_ports = midi_out.ports();
-    let out_port: &MidiOutputPort = match out_ports.len() {
-        0 => return,
-        1 => {
-            println!("Choosing the only available output port: {}", midi_out.port_name(&out_ports[0]).unwrap());
-            &out_ports[0]
-        },
-        _ => {
-            println!("\nAvailable output ports:");
-            for (i, p) in out_ports.iter().enumerate() {
-                println!("{}: {}", i, midi_out.port_name(p).unwrap());
-            }
-            print!("Please select output port: ");
-            let mut input = String::new();
-            stdin().read_line(&mut input).unwrap();
-            out_ports.get(input.trim().parse::<usize>().unwrap())
-                     .ok_or("invalid output port selected").unwrap()
+    let midi_out  = MidiOutput::new("Isomorphic Keyboard Output").unwrap();
+    let out = if let Some(o) = opts.midi.output {
+        midi_out.ports().get(o.trim().parse::<usize>().unwrap())
+            .ok_or_else(|| "Invalid port")
+            .and_then(|p| {
+                println!("Connecting to port {}", midi_out.port_name(p).unwrap());
+                midi_out.connect(p, "Isomorphic Keyboard").map_err(|_| "Unable to connect")
+            })
+    } else if opts.midi.use_virtual {
+        Err("Virtual not available")
+        //midi_out.create_virtual("Isomorphic Keyboard")
+    } else if opts.midi.list_outputs {
+        println!("Available MIDI outputs");
+        for (i, p) in midi_out.ports().iter().enumerate() {
+            println!("{}: {}", i, midi_out.port_name(p).unwrap());
         }
+        Err("")
+    } else {
+        println!("Available MIDI outputs");
+        for (i, p) in midi_out.ports().iter().enumerate() {
+            println!("{}: {}", i, midi_out.port_name(p).unwrap());
+        }
+        Err("No output selected")
     };
 
-    let mut conn_out = midi_out.connect(out_port, "midir-test").unwrap();
+    if out.is_err() {
+        println!("{}", out.err().unwrap());
+        return;
+    }
+    let mut conn_out = out.unwrap();
 
     let basis_vector = match opts.basis.preset.as_ref().map(String::as_ref) {
         Some("janko") =>  JANKO,
@@ -104,24 +126,25 @@ fn main() {
         Some("park") =>  PARK,
         Some("wesley") =>  WESLEY,
         Some("fernandez") =>  FERNANDEZ,
-        Some("wicki_haydn") =>  WICKI_HAYDN,
+        Some("wicki_hayden") =>  WICKI_HAYDEN,
         Some("b_system") =>  B_SYSTEM,
         Some("c_system") =>  C_SYSTEM,
+        Some("bosanquet") =>  BOSANQUET,
         _ => opts.basis.specified,
     };
 
     let basis = if basis_vector.rotation == -90 {
-        println!("vertical");
+        println!("vertical {} {}", basis_vector.upper, basis_vector.lower);
         BasisVector(basis_vector.upper, basis_vector.lower)
     } else {
         // Rotate -60 degrees to align with vertical
-        println!("horizontal");
+        println!("horizontal {} {}", basis_vector.upper, basis_vector.lower);
         BasisVector(basis_vector.upper - basis_vector.lower , basis_vector.upper)
     };
 
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
     // Create a windowed mode window and its OpenGL context
-    let (mut window, events) = glfw.create_window(300, 300, "hello world!", glfw::WindowMode::Windowed)
+    let (mut window, events) = glfw.create_window(300, 300, "Isomorphic MIDI Input", glfw::WindowMode::Windowed)
         .expect("Failed to create GLFW window.");
 
     // Make the window's context current
