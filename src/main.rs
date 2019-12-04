f081b2487f52d3119801afa7cdf8d60047a78244
Ashton Snelgrove <ashton@snelgrove.science>
2019-12-03 18:29:09 -0700

    Fix basis vectors and add presets

--- src/main.rs
+++ src/main.rs
@@ -5,32 +5,65 @@ extern crate midir;
 mod keycodes;
 mod points;
 
+use structopt::StructOpt;
 use points::{BasisVector, Interval, point_to_interval};
 use keycodes::key_to_point;
 use glfw::{Action, Context, Key};
 use midir::{MidiOutput, MidiOutputPort};
 use std::io::stdin;
-use structopt::StructOpt;
 
-// something else interesting 2, -1, 0
-// Janko 2, 1, 0
-// Inverted Gerhard (1, -3, 0) (4, 1, -60)
-// Gerhard 3, -1, 30
-// Tonnetz 4, -3, -30
-// Guitar A 1, -4, 0
-// Guitar B 1, -5, 0
-// Park 2, -3, 30
-// Wellesy 5, 13, 30
-// Wicki-Haydn 2, 7, 0
+// something else interesting [1, 3, -90] haha reinvented b-system in a different orientation
+// Bosanquet diagram [3, 2, 0]
+// Janko [-1, 1, -90]
+// Inverted Gerhard [3, 4, -90] note i also liked this mapped on the keyboard rotated -60 [-1, 3, -90]
+// Gerhard [4, 3, 0]
+// Tonnetz [4, -3, 0]
+// Guitar A [4, 5, -90]
+// Guitar B [5, 6, -90]
+// Park [5, 2, 0]
+// Wesley [7, -5, 0]
+// Fernandez [6, 7, -90]
+// Wicki-Haydn [5, 7, -90]
+// B-system [-2, 1, -90]
+// C-system [-1, 2, -90]
 
+#[derive(StructOpt)]
+struct Basis {
+    #[structopt(short, long, default_value = "4")]
+    upper: i8,
+    #[structopt(short, long, default_value = "-3")]
+    lower: i8,
+    #[structopt(short, long, default_value = "-90")]
+    rotation: i8
+}
+
+const JANKO: Basis            = Basis { upper: -1, lower: 1,  rotation: -90 };
+const INVERTED_GERHARD: Basis = Basis { upper: 3,  lower: 4,  rotation: -90 };
+const GERHARD: Basis          = Basis { upper: 4,  lower: 3,  rotation: 0 };
+const TONNETZ: Basis          = Basis { upper: 4,  lower: -3, rotation:  0 };
+const GUITAR_A: Basis         = Basis { upper: -1, lower: 1,  rotation: -90 };
+const GUITAR_B: Basis         = Basis { upper: -1, lower: 1,  rotation: -90 };
+const PARK: Basis             = Basis { upper: 5,  lower: 2,  rotation: -90 };
+const WESLEY: Basis           = Basis { upper: 7,  lower: -5, rotation: 0};
+const FERNANDEZ: Basis        = Basis { upper: 6,  lower: 7,  rotation: -90 };
+const WICKI_HAYDN: Basis      = Basis { upper: 5,  lower: 7,  rotation: -90 };
+const B_SYSTEM: Basis         = Basis { upper: -2, lower: 1,  rotation: -90 };
+const C_SYSTEM: Basis         = Basis { upper: -1, lower: 2,  rotation: -90 };
+const NAMES: &'static [&'static str] = &["C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B"];
+
+#[derive(StructOpt)]
+struct BasisPreset {
+    #[structopt(short, long)]
+    preset: Option<String>,
+    #[structopt(flatten)]
+    specified: Basis,
+}
 
 #[derive(StructOpt)]
 #[structopt(name = "Isomorphic Keyboard", version = "1.0", author = "Ashton Snelgrove")]
 struct Opts {
-    #[structopt(short = "a", long = "basis-up", default_value = "4")]
-    basis_up: i8,
-    #[structopt(short = "b", long = "basis-up-right", default_value = "-3")]
-    basis_up_right: i8,
+    #[structopt(flatten)]
+    basis: BasisPreset,
     #[structopt(short, long, default_value = "0")]
     transpose: i8,
 }
@@ -61,8 +94,30 @@ fn main() {
 
     let mut conn_out = midi_out.connect(out_port, "midir-test").unwrap();
 
-    let names = vec!["C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B"];
-    let basis = BasisVector(opts.basis_up, opts.basis_up_right);
+    let basis_vector = match opts.basis.preset.as_ref().map(String::as_ref) {
+        Some("janko") =>  JANKO,
+        Some("inverted_gerhard") =>  INVERTED_GERHARD,
+        Some("gerhard") =>  GERHARD,
+        Some("tonnetz") =>  TONNETZ,
+        Some("guitar_a") =>  GUITAR_A,
+        Some("guitar_b") =>  GUITAR_B,
+        Some("park") =>  PARK,
+        Some("wesley") =>  WESLEY,
+        Some("fernandez") =>  FERNANDEZ,
+        Some("wicki_haydn") =>  WICKI_HAYDN,
+        Some("b_system") =>  B_SYSTEM,
+        Some("c_system") =>  C_SYSTEM,
+        _ => opts.basis.specified,
+    };
+
+    let basis = if basis_vector.rotation == -90 {
+        println!("vertical");
+        BasisVector(basis_vector.upper, basis_vector.lower)
+    } else {
+        // Rotate -60 degrees to align with vertical
+        println!("horizontal");
+        BasisVector(basis_vector.upper - basis_vector.lower , basis_vector.upper)
+    };
 
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
     // Create a windowed mode window and its OpenGL context
@@ -76,7 +131,7 @@ fn main() {
     // Loop until the user closes the window
     while !window.should_close() {
         // Swap front and back buffers
-        //        window.swap_buffers();
+        //window.swap_buffers();
 
         // Poll for and process events
         glfw.wait_events();
@@ -105,11 +160,11 @@ fn main() {
                             match a {
                                 Action::Press => {
                                     let _ = conn_out.send(&[0x90, note, 64]);
-                                    println!("{}{} {} on",  names[name as usize], octave, note);
+                                    println!("{}{} {} on",  NAMES[name as usize], octave, note);
                                 },
                                 Action::Release => {
                                     let _ = conn_out.send(&[0x80, note, 0]);
-                                    println!("{}{} {} off",  names[name as usize], octave, note);
+                                    println!("{}{} {} off",  NAMES[name as usize], octave, note);
                                 },
                                 _ => {}
                             }
--- src/points.rs
+++ src/points.rs
@@ -10,7 +10,7 @@ pub struct Interval(pub i8);
 pub fn point_to_interval(basis: &BasisVector, point: &Point) -> Interval {
     let BasisVector(bu, bv) = basis;
     let Point(u, v) = point;
-    Interval(bu * u + bv * v)
+    Interval(bu * v + (bv - bu) * u)
 }
 
 #[cfg(test)]
@@ -19,27 +19,30 @@ mod tests {
 
     #[test]
     fn interval_at_the_origin_is_zero() {
-        assert_eq!(point_to_interval(BV(3, 4), P(0, 0)), I(0));
+        assert_eq!(point_to_interval(&BV(3, 4), &P(0, 0)), I(0));
     }
 
     #[test]
     fn interval_one_step_away() {
-        assert_eq!(point_to_interval(BV(3, 4), P(-1, 1)), I(1));
-        assert_eq!(point_to_interval(BV(3, 4), P(0, -1)), I(-4));
-        assert_eq!(point_to_interval(BV(3, 4), P(-1, 0)), I(-3));
-        assert_eq!(point_to_interval(BV(3, 4), P(1, 0)), I(3));
-        assert_eq!(point_to_interval(BV(3, 4), P(0, 1)), I(4));
-        assert_eq!(point_to_interval(BV(3, 4), P(1, -1)), I(-1));
+        assert_eq!(point_to_interval(&BV(3, 4), &P(0, 1)), I(3));
+        assert_eq!(point_to_interval(&BV(3, 4), &P(-1, 0)), I(-1));
+        assert_eq!(point_to_interval(&BV(3, 4), &P(1, 0)), I(1));
+        assert_eq!(point_to_interval(&BV(3, 4), &P(0, -1)), I(-3));
+        assert_eq!(point_to_interval(&BV(3, 4), &P(-1, -1)), I(-4));
+        assert_eq!(point_to_interval(&BV(3, 4), &P(1, 1)), I(4));
     }
 
     #[test]
     fn interval_several_step_away() {
-        assert_eq!(point_to_interval(BV(3, 4), P(0, 2)), I(8));
-        assert_eq!(point_to_interval(BV(3, 4), P(2, 0)), I(6));
-        assert_eq!(point_to_interval(BV(3, 4), P(1, 1)), I(7));
-        assert_eq!(point_to_interval(BV(3, 4), P(3, 1)), I(13));
-        assert_eq!(point_to_interval(BV(3, 4), P(-3, -1)), I(-13));
-        assert_eq!(point_to_interval(BV(3, 4), P(-1, 2)), I(5));
+        assert_eq!(point_to_interval(&BV(3, 4), &P(0, 2)), I(6));
+        assert_eq!(point_to_interval(&BV(3, 4), &P(0, -2)), I(-6));
+        assert_eq!(point_to_interval(&BV(3, 4), &P(2, 0)), I(2));
+        assert_eq!(point_to_interval(&BV(3, 4), &P(-2, 0)), I(-2));
+        assert_eq!(point_to_interval(&BV(3, 4), &P(1, 2)), I(7));
+        assert_eq!(point_to_interval(&BV(3, 4), &P(3, 1)), I(6));
+        assert_eq!(point_to_interval(&BV(3, 4), &P(-3, 1)), I(0));
+        assert_eq!(point_to_interval(&BV(3, 4), &P(-3, -1)), I(-6));
+        assert_eq!(point_to_interval(&BV(3, 4), &P(-1, 2)), I(5));
     }
 
 }
